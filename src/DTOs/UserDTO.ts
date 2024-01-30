de8d86ca17f27ec20814727a06c9281c3fe06417
Thiago <thiagomvas@gmail.com>
2024-01-29 22:48:50 -0300

    Add UserDTO

---
+++ src/DTOs/UserDTO.ts
@@ -0,0 +1,11 @@
+type UserDTO = {
+    id: string;
+    name: string;
+    password: string;
+    email: string;
+    role: string;
+    phonenumber: string;
+    address: string;
+}
+
+export default UserDTO;
