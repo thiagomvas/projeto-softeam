afce914ded7fab53e9eac90f48da8c5d01e5ef3a
Thiago <thiagomvas@gmail.com>
2024-02-05 23:57:50 -0300

    Add mapping to some DTOs

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,39 @@
+// utils.ts
+
+import ClassDTO from "./DTOs/ClassDTO";
+import DisciplineDTO from "./DTOs/DisciplineDTO";
 import UserDTO from "./DTOs/UserDTO";
 
-
-export function mapResponseToUserDTO (responseData: any): UserDTO {
-    return {
-      id: responseData.id,
-      name: responseData.name,
-      password: responseData.password,
-      email: responseData.email,
-      role: responseData.role,
-      phonenumber: responseData.phonenumber,
-      address: responseData.address,
-    };
+export function mapResponseToUserDTO(responseData: any): UserDTO {
+  const userDTO: UserDTO = {
+    id: responseData.id,
+    fullname: responseData.fullname,
+    email: responseData.email,
+    password: responseData.password,
+    phonenumber: responseData.phonenumber,
+    address: responseData.address,
+    role: responseData.role,
   };
+  return userDTO;
+}
 
-  export default mapResponseToUserDTO;
+export function mapResponseToClassDTO(responseData: any): ClassDTO {
+  const classDTO: ClassDTO = {
+    id: responseData.id,
+    disciplineId: responseData.disciplineId,
+    professorId: responseData.professorId,
+    classTimes: responseData.classtimes,
+    roomNumber: responseData.roomNumber,
+  };
+  return classDTO;
+}
+
+export function mapResponseToDisciplineDTO(responseData: any): DisciplineDTO {
+  const disciplineDTO: DisciplineDTO = {
+    id: responseData.id,
+    name: responseData.name,
+    description: responseData.description,
+    department: responseData.department,
+  };
+  return disciplineDTO;
+}
